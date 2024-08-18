a963651203471af3bb08cb31183683e7b5f69aca
moontai0724 <moontai0724@gmail.com>
2024-08-18 22:33:19 +0800

    refactor: use shorthand symbols

--- composables/useKeycloak.ts
+++ composables/useKeycloak.ts
@@ -5,21 +5,20 @@ import { useKeycloakStore } from "~/stores/keycloak";
 export function useKeycloak() {
   const keycloakStore = useKeycloakStore();
   const { isAuthenticated, userProfile } = storeToRefs(keycloakStore);
+  const { login, logout, initKeycloak } = keycloakStore;
 
   const checkAuthAndRedirect = async () => {
-    await keycloakStore.initKeycloak();
-    if (!keycloakStore.isAuthenticated) {
-      keycloakStore.login();
-    }
+    await initKeycloak();
+    if (!keycloakStore.isAuthenticated) login();
   };
 
   return {
     keycloak: keycloakStore.keycloak,
     isAuthenticated,
     userProfile,
-    login: keycloakStore.login,
-    logout: keycloakStore.logout,
-    initKeycloak: keycloakStore.initKeycloak,
+    login,
+    logout,
+    initKeycloak,
     checkAuthAndRedirect,
   };
 }
